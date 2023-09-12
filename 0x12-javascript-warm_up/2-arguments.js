e9a8b800035d14d4337bca1e8c6d5f8ab6fe8efc
Oziegbe Daniel <oziegbeosy@gmail.com>
2023-09-12 17:50:11 -0400

    javascripts updated

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,8 +1,6 @@
 #!/usr/bin/node
 
-const args = process.argv.slice(2);
-
-if (args.length === 0) {
+if (process.argv.length === 0) {
   console.log('No Argument');
 } else {
   console.log('Argument found');
