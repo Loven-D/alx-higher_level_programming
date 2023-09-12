1ae2c293bef3286104afc4797d9fb6eb99018ddc
Oziegbe Daniel <oziegbeosy@gmail.com>
2023-09-12 17:34:42 -0400

    javascripts updated

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+const args = process.argv.slice(2);
+
+if (args.length === 0) {
+  console.log('No Argument');
+} else {
+  console.log('Argument found');
+}
---
+++ 0x12-javascript-warm_up/README.md
@@ -0,0 +1,2 @@
+This task is a warm up to learning javascript.
+ALX task to becoming better fullstack engineer.
