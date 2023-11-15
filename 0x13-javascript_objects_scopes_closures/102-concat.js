a51c668fb9c3b35126f87c821f2750aba2582831
Oziegbe Daniel <oziegbeosy@gmail.com>
2023-11-15 09:35:59 -0500

    JS advance task

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,28 +1,8 @@
 #!/usr/bin/node
 
 const fs = require('fs');
+let content = '';
 
-if (process.argv.length !== 5) {
-  console.error('Usage: ./102-concat.js fileA fileB fileC');
-  process.exit(1);
-}
-
-const [, , sourceFileA, sourceFileB, destinationFile] = process.argv;
-
-try {
-  // console.log(`Reading contents of ${sourceFileA}...`);
-  const contentA = fs.readFileSync(sourceFileA, 'utf8');
-  // console.log(`Contents of ${sourceFileA}: ${contentA}`);
-
-  const contentB = fs.readFileSync(sourceFileB, 'utf8');
-  // console.log(`Contents of ${sourceFileB}: ${contentB}`);
-
-  const concatenatedContent = contentA + '\n' + contentB;
-
-  fs.writeFileSync(destinationFile, concatenatedContent);
-
-  // console.log(`Files "${sourceFileA}" and "${sourceFileB}" concatenated successfully to "${destinationFile}".`);
-} catch (error) {
-  console.error('Error:', error.message);
-  process.exit(1);
-}
+content = content.concat(fs.readFileSync(process.argv[2]));
+content = content.concat(fs.readFileSync(process.argv[3]));
+fs.writeFileSync(process.argv[4], content);
